3c874af324f28ac3950fc891b235c6f100da1b66
Ezequiel García <ezeezegg@gmail.com>
2015-01-04 09:31:39 -0600

    initial project login with facebook and tw

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'authLaravelSimple',
+			'database'  => 'authLaravelSocialMedia',
 			'username'  => 'root',
 			'password'  => 'root',
 			'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -10,7 +10,8 @@ class UserTableSeeder extends Seeder {
 			'last_name'  => 'Gonzalez Garcia',
 			'username'   => 'ezeezegg',
 			'email'      => 'ezeezegg@gmail.com',
-			'password'   =>  Hash::make('admin')
+			//'password'   =>  Hash::make('admin')
+			'password'   =>  'admin' //no es necesario porque en el modelo user ya lo hace
 			]);
 		}
 
